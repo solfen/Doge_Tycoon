f927410051cca4d7f2de692a21c72ae315a5de2a
Lucien Boudy <luboudy@gmail.com>
2015-04-16 16:59:14 +0200

    api update

--- Social_City_Builder/php/api/config.php
+++ Social_City_Builder/php/api/config.php
@@ -1,12 +1,17 @@
 <?php
 
-	$src = 'mysql:host=10.1.100.18;dbname=dogeexplorer';
+	/*$src = 'mysql:host=10.1.100.18;dbname=dogeexplorer';
+	//$src = 'mysql:host=fbgame.isartdigital.com;dbname=dogeexplorer';
 	$user = 'dogeexplorer';
-	$pwd = 'doge2015';
+	$pwd = 'doge2015';*/
 
-	function error () {
+	$src = 'mysql:host=127.0.0.1;dbname=doge_tycoon';
+	$user = 'root';
+	$pwd = '';
 
-		echo 0;
+	function error ($error_msg) {
+
+		echo '{"error":"'.$error_msg.'"}';
 		die();
 	}
 ?>
